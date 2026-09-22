2fb489527245692b3d7e62d9774049afab186324
hour alhejress <houralhejress@gmail.com>
2026-09-22 09:53:05 +0300

    lab3

--- Lab2/libraryproject/settings.py
+++ Lab2/libraryproject/settings.py
@@ -9,11 +9,12 @@ https://docs.djangoproject.com/en/6.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
 
 
 # Quick-start development settings - unsuitable for production
@@ -53,10 +54,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'libraryproject.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, "apps" + os.sep + "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
