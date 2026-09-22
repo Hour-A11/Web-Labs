a97984500acaca76d786ea07ec4bd309d521dc30
hour alhejress <houralhejress@gmail.com>
2026-09-22 11:15:10 +0300

    complete lab4

--- Lab2/apps/bookmodule/views.py
+++ Lab2/apps/bookmodule/views.py
@@ -9,15 +9,10 @@ def index2(request, val1=0):
     return HttpResponse("value1 = " + str(val1))
 
 def viewbook(request, bookId):
-    # assume that we have the following books somewhere (e.g. database)
-    book1 = {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
-    book2 = {'id': 456, 'title': 'Secrets of Reverse Engineering', 'author': 'E. Eilam'}
+    return render(request, 'bookmodule/one_book.html')
 
-    targetBook = None
-    if book1['id'] == bookId:
-        targetBook = book1
-    if book2['id'] == bookId:
-        targetBook = book2
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
 
-    context = {'book': targetBook}
-    return render(request, 'bookmodule/show.html', context)
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
